45302a1da26980d53a61f3dfa71697edb1e17a65
rjansen <raphael.jansen@e-pedion.com>
2016-10-23 20:54:42 -0200

    Add warning when the setup function raises error

--- context.go
+++ context.go
@@ -1,10 +1,19 @@
 package context
 
+import (
+	"farm.e-pedion.com/repo/logger"
+)
+
 //Setup calls all provided setup functions and return all raised errors
 func Setup(setupFuncs ...SetupFunc) []error {
 	var errs []error
-	for _, v := range setupFuncs {
+	for i, v := range setupFuncs {
 		if err := v(); err != nil {
+			logger.Warn("contex.Setup",
+				logger.Int("index", i),
+				logger.Struct("func", v),
+				logger.Err(err),
+			)
 			errs = append(errs, err)
 		}
 	}
